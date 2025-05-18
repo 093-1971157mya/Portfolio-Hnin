00dd75e49c300af8661656ed64c62ef799725690
093-1971157mya <132291393+093-1971157mya@users.noreply.github.com>
2025-05-18 14:37:22 +0700

    added

--- App.js
+++ App.js
@@ -9,12 +9,12 @@ export default function App() {
         <h1>Weather App</h1>
         <footer>
         This project was coded by ["HTUN THANDAR NAING"]
-        <a haref="https://www.myathandar.io" target="_blank" >
+        <a haref="https://app.netlify.com/projects/weatherappfaisallove/overview" target="_blank" >
             MYA THANDAR
         </a>>{""}
         <a href="">
             and is{""}
-            <a href="" target="_blank">
+            <a href="https://github.com/093-1971157mya/Portfolio-Hnin" target="_blank">
             open-source code on Github
         </a>
         </footer>
