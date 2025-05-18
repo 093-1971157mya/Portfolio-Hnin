f2f6eebda92af448dc7aacfe423af5454571ecac
093-1971157mya <132291393+093-1971157mya@users.noreply.github.com>
2025-05-18 14:28:55 +0700

    added

---
+++ App.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+import "./App.css";
+export default function App() {
+
+
+    return (
+        <div class name="App">
+        <h1>Weather App</h1>
+        <footer>
+        This project was coded by ["HTUN THANDAR NAING"]
+        <a haref="https://www.myathandar.io" target="_blank" >
+            MYA THANDAR
+        </a>>{""}
+        <a href="">
+            and is{""}
+            <a href="" target="_blank">
+            open-source code on Github
+        </a>
+        </footer>
+    </div>
+            
